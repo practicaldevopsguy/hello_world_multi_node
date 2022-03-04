2e2b0c45c033d6ad00b41d8f4c41dde25bc5d233
Zoli <zolivadasz@gmail.com>
2022-03-04 10:15:44 +0100

    build.sh locking

--- cicd_docker_image/build.sh
+++ cicd_docker_image/build.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/bash
 
+[ -d "/work/build.lock" ] && echo "Build is running, exiting... " ; exit 1
+touch /work/build.lock
+
 echo $(date +"%Y-%m-%d %T") - Build script started
 
 
-[ ! -d "/work/hello_world" ] && cd cd /work ; git clone ssh://cicd@192.168.1.104:/DATA/git/hello_world
+
+
+[ ! -d "/work/hello_world" ] && cd /work ; git clone ssh://cicd@192.168.1.104:/DATA/git/hello_world
 [ -d "/work/hello_world" ] && cd /work/hello_world ; git pull
 
 
 cp /work/hello_world/cicd_docker_image/build.sh /work/build.sh
 
+
 echo $(date +"%Y-%m-%d %T") - DONE
 echo
+rm /work/build.lock
