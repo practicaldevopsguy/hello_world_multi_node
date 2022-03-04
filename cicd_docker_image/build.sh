73c9ad55e11fa37fc30a5dbe1976aea8cd2f56b9
Zoli <zolivadasz@gmail.com>
2022-03-04 10:29:11 +0100

    build.sh locking

--- cicd_docker_image/build.sh
+++ cicd_docker_image/build.sh
@@ -12,7 +12,7 @@ echo $(date +"%Y-%m-%d %T") - Build script started
 [ -d "/work/hello_world" ] && cd /work/hello_world ; git pull
 
 
-#cp /work/hello_world/cicd_docker_image/build.sh /work/build.sh
+cp /work/hello_world/cicd_docker_image/build.sh /work/build.sh
 
 
 echo $(date +"%Y-%m-%d %T") - DONE
