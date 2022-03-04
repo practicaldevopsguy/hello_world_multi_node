9fff75a1314b66c96597e889a1429039e9e8f950
Zoli <zolivadasz@gmail.com>
2022-03-04 10:11:46 +0100

    build.sh

--- cicd_docker_image/build.sh
+++ cicd_docker_image/build.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/bash
 
-date +"%Y-%m-%d %T"
-echo Build script started
+echo $(date +"%Y-%m-%d %T") - Build script started
 
 
-cd /work
+[ ! -d "/work/hello_world" ] && cd cd /work ; git clone ssh://cicd@192.168.1.104:/DATA/git/hello_world
+[ -d "/work/hello_world" ] && cd /work/hello_world ; git pull
 
-git clone ssh://cicd@192.168.1.104:/DATA/git/hello_world
 
+cp /work/hello_world/cicd_docker_image/build.sh /work/build.sh
+
+echo $(date +"%Y-%m-%d %T") - DONE
+echo
